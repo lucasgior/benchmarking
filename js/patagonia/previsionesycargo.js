bb7f679deb5878febadf5f8308c80bcdb9cc6792
lucasgior <lucasg_03@hotmail.com>
2015-04-07 01:43:39 -0300

    Create previsionesycargo.js

---
+++ js/patagonia/previsionesycargo.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#previsionesycargo').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' '
+        },
+        subtitle: {
+            text: ' '
+        },
+        xAxis: {
+            categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            title: {
+                text: ' '
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Previsiones',
+            data: [163,226,497,950,985,1012,1074,1131]
+        },{
+            name: 'Cargo',
+            data: [53,97,301,525,57,133,227,316]
+        }]
+    });
+});
