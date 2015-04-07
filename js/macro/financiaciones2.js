9335917aed52050c37271850b7d096705773dea4
lucasgior <lucasg_03@hotmail.com>
2015-04-07 01:39:55 -0300

    Create financiaciones2.js

---
+++ js/macro/financiaciones2.js
@@ -0,0 +1,64 @@
+
+$(function () {
+    $('#financ2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+           categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total de Financiaciones'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'center',
+            x: 0,
+            verticalAlign: 'down',
+            y: -10,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Comercial',
+            data: [7206,10712,12816,15078,15557,15213,15359,14881]
+        }, {
+            name: 'Automática',
+            data: [8277,12628,16836,21682,21481,21902,23831,26593]
+        }]
+    });
+});
