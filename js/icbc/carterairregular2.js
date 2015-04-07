b1479187c3e40c4c3ec46279a24dc84a808db6a4
lucasgior <lucasg_03@hotmail.com>
2015-04-07 00:55:11 -0300

    Create carterairregular2.js

---
+++ js/icbc/carterairregular2.js
@@ -0,0 +1,96 @@
+$(function () {
+
+    var colors = Highcharts.getOptions().colors,
+        categories = ['Por Tipo de Cartera'],
+        data = [ 
+            
+            {
+            y: 331,
+            color: colors[1],
+            drilldown: {
+                name: 'Sit.Irreg.',
+                categories: ['Comercial', 'Automática'],
+                data: [50,281],
+                color: colors[0]
+            }
+        },  
+           ],
+        browserData = [],
+        versionsData = [],
+        i,
+        j,
+        dataLen = data.length,
+        drillDataLen,
+        brightness;
+
+
+    // Build the data arrays
+    for (i = 0; i < dataLen; i += 1) {
+
+        // add browser data
+        browserData.push({
+            name: categories[i],
+            y: data[i].y,
+            color: data[i].color
+        });
+
+        // add version data
+        drillDataLen = data[i].drilldown.data.length;
+        for (j = 0; j < drillDataLen; j += 1) {
+            brightness = 0.2 - (j / drillDataLen) / 5;
+            versionsData.push({
+                name: data[i].drilldown.categories[j],
+                y: data[i].drilldown.data[j],
+                color: Highcharts.Color(data[i].color).brighten(brightness).get()
+            });
+        }
+    }
+
+    // Create the chart
+    $('#carterairregular2').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        yAxis: {
+            title: {
+                text: ''
+            }
+        },
+        plotOptions: {
+            pie: {
+                shadow: false,
+                center: ['50%', '50%']
+            }
+        },
+        tooltip: {
+            valueSuffix: 'MM'
+        },
+        series: [{
+            name: 'Total',
+            data: browserData,
+            size: '60%',
+            dataLabels: {
+                formatter: function () {
+                    return this.y > 5 ? this.point.name : null;
+                },
+                color: 'white',
+                distance: -100
+            }
+        }, {
+            name: 'Valor',
+            data: versionsData,
+            size: '80%',
+            innerSize: '60%',
+            dataLabels: {
+                formatter: function () {
+                    // display only if larger than 1
+                    return this.y > 1 ? '<b>' + this.point.name + '</b> '  : null;
+                },
+            distance: -25
+            }
+        }]
+    });
+});
