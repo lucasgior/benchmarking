cbfd03437555fabd3a36895e52b0c7dd11e50225
lucasgior <lucasg_03@hotmail.com>
2015-04-07 00:48:52 -0300

    Create previsionesycargo.js

---
+++ js/galicia/previsionesycargo.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#previsionesycargo').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' '
+        },
+        subtitle: {
+            text: ' '
+        },
+        xAxis: {
+            categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            title: {
+                text: ' '
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Previsiones',
+            data: [656,811,917,1052,1173,1265,1392,1365]
+        },{
+            name: 'Cargo',
+            data: [307,493,593,820,359,650,1045,1265]
+        }]
+    });
+});
