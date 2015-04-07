0c370ebf90806bdfb01446e2a02323e856850667
lucasgior <lucasg_03@hotmail.com>
2015-04-07 00:46:09 -0300

    Create previsionesycargo.js

---
+++ js/credicoop/previsionesycargo.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#previsionesycargo').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' '
+        },
+        subtitle: {
+            text: ' '
+        },
+        xAxis: {
+            categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            title: {
+                text: ' '
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Previsiones',
+            data: [286,377,430,556,559,574,594,645]
+        },{
+            name: 'Cargo',
+            data: [202,100,98,184,224,282,91,207]
+        }]
+    });
+});
