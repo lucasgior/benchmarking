fa223cb0b2a4ac63e0e77b1f0c787bce7da294a9
lucasgior <lucasg_03@hotmail.com>
2015-04-07 01:47:02 -0300

    Create previsionesycargo.js

---
+++ js/provincia/previsionesycargo.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#previsionesycargo').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' '
+        },
+        subtitle: {
+            text: ' '
+        },
+        xAxis: {
+            categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            title: {
+                text: ' '
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Previsiones',
+            data: [479,580,854,1097,1302,1397,1509,1581]
+        },{
+            name: 'Cargo',
+            data: [0,0,329,398,242,385,543,677]
+        }]
+    });
+});
