14f903a7e0f4f0f1ea0660c7cdda9bca281f3ac1
lucasgior <lucasg_03@hotmail.com>
2015-04-07 01:40:04 -0300

    Create financiaciones1.js

---
+++ js/macro/financiaciones1.js
@@ -0,0 +1,66 @@
+$(function () {
+    $('#financ1').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+           categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total de Financiaciones'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'center',
+            x: 0,
+            verticalAlign: 'down',
+            y: -10,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Sect. Privado',
+            data: [15484,23340,29653,36760,37038,37116,39190,41475]
+        }, {
+            name: 'Sect. Público',
+            data: [335,335,586,639,693,672,639,604]
+        }, {
+            name: 'Sect. Financiero',
+            data: [78,431,177,334,295,804,201,108]
+        }]
+    });
+});
