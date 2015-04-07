ca305bc656ee5708715fb50cfc59a58edc3bc4ac
lucasgior <lucasg_03@hotmail.com>
2015-04-07 00:54:02 -0300

    Create previsionesycargo.js

---
+++ js/hsbc/previsionesycargo.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#previsionesycargo').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' '
+        },
+        subtitle: {
+            text: ' '
+        },
+        xAxis: {
+            categories: ['Dic-10', 'Dic-11', 'Dic-12', 'Dic-13', 'Mar-14', 'Jun-14', 'Sep-14', 'Dic-14']
+        },
+        yAxis: {
+            title: {
+                text: ' '
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Previsiones',
+            data: [215,288,406,506,589,594,620,671]
+        },{
+            name: 'Cargo',
+            data: [169,125,179,219,387,476,107,243]
+        }]
+    });
+});
